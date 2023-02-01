9df375fb45ea9bd38dc3445eaf63d9323ff260b5
billal <karimovbillal20002@gmail.com>
2023-02-02 00:11:04 +0600

    init project

---
+++ parser_app/paginator.py
@@ -0,0 +1,10 @@
+from configs.utils import RequestTranslator
+
+
+class Paginator(RequestTranslator):
+    def find_button_next(self) -> bool:
+        """
+        показывает, имеется ли кнопка next на странице
+        """
+        pagination = self._get_soup().find('div', class_='pagination')
+        return 'Next' in pagination.text
